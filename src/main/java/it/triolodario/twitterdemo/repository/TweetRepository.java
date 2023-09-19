6f3f0278595bb0414671e22535760bb72634e73b
dario.triolo <>
2023-09-19 14:37:06 +0200

    unit testing repository layer

--- src/main/java/it/triolodario/twitterdemo/repository/TweetRepository.java
+++ src/main/java/it/triolodario/twitterdemo/repository/TweetRepository.java
@@ -1,5 +1,7 @@
 package it.triolodario.twitterdemo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import it.triolodario.twitterdemo.model.Tweet;
 
 @Repository
 public interface TweetRepository extends JpaRepository<Tweet, Long> {
+	
+	List<Tweet> findByAuthor(String author);
 
 }
--- src/test/java/it/triolodario/twitterdemo/repository/TweetRepositoryTest.java
+++ src/test/java/it/triolodario/twitterdemo/repository/TweetRepositoryTest.java
@@ -26,6 +26,7 @@ public class TweetRepositoryTest {
 	public void TweetRepositorySaveAllShouldReturnSavedTweets() {
 
 		// Arrange
+		tweets.clear();
 		tweets.add(new Tweet("AuthorA", "MessageA", Instant.now()));
 		tweets.add(new Tweet("AuthorB", "MessageB", Instant.now()));
 
@@ -35,6 +36,49 @@ public class TweetRepositoryTest {
 		// Assert
 		Assertions.assertThat(savedTweets).isNotNull();
 		Assertions.assertThat(savedTweets).hasSize(2);
+
+	}
+
+	@Test
+	public void TweetRepositoryGetAllShouldReturnAllTweets() {
+
+		final String authorA = "AuthorA";
+
+		// Arrange
+		tweets.clear();
+		tweets.add(new Tweet(authorA, "MessageA", Instant.now()));
+		tweets.add(new Tweet("AuthorB", "MessageB", Instant.now()));
+
+		// Act
+		tweetRepository.saveAll(tweets);
+
+		// Assert
+		List<Tweet> tweetsList = tweetRepository.findAll();
+		Assertions.assertThat(tweetsList).isNotNull();
+		Assertions.assertThat(tweetsList).hasSize(2);
+		Assertions.assertThat(tweetsList.get(0).getAuthor()).isEqualTo(authorA);
+
+	}
+	
+	@Test
+	public void TweetRepositoryFindByAuthorShouldReturnAllTweetsByAuthor() {
+
+		final String authorC = "AuthorC";
+
+		// Arrange
+		tweets.clear();
+		tweets.add(new Tweet("authorA", "MessageA", Instant.now()));
+		tweets.add(new Tweet("AuthorB", "MessageB", Instant.now()));
+		tweets.add(new Tweet(authorC, "Messagec", Instant.now()));
+
+		// Act
+		tweetRepository.saveAll(tweets);
+
+		// Assert
+		List<Tweet> tweetsList = tweetRepository.findByAuthor(authorC);
+		Assertions.assertThat(tweetsList).isNotNull().hasSize(1);
+		Assertions.assertThat(tweetsList.get(0).getAuthor()).isEqualTo(authorC);
+		Assertions.assertThat(tweetsList.get(0).getMessage()).isEqualTo("Messagec");
 
 	}
 
